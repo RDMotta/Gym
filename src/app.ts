a660065972f4c7109b477c37881cbd8fb84484d7
RDMotta <muitopoco@gmail.com>
2023-11-20 19:35:28 -0300

    add global error validations

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,24 @@
 import fastify from "fastify";
+import { ZodError } from "zod";
 import { appRoutes } from "./http/routes";
 
 export const app = fastify();
-app.register(appRoutes)
+app.register(appRoutes);
+
+app.setErrorHandler((error, _, reply) => {
+    if (error instanceof ZodError) {
+        return reply
+            .status(400)
+            .send({
+                message: 'Validation.',
+                content: error.format()
+            })
+    }
+
+    return reply
+        .status(400)
+        .send({
+            message: 'Internal server error.',
+            content: error.message
+        })
+})
